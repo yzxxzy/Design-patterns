161d666322a46880b07641f387560ade9cbb4baa
姚增轩 <xzynuli66@163.com>
2020-12-09 10:52:21 +0800

    初始化项目

---
+++ src/main/java/com/chenyi/design/designpatterns/DesignpatternsApplication.java
@@ -0,0 +1,13 @@
+package com.chenyi.design.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignpatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chenyi/design/designpatterns/DesignpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenyi.design.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
